9cf2f50606091bb56d4b1aeeb6dd44edcab25709
Ben Vining <ben.the.vining@gmail.com>
2022-06-12 00:56:13 -0500

    updating Makefile

--- Makefile
+++ Makefile
@@ -1,1 +1,50 @@
-# configure cmake
+SHELL := /bin/sh
+.ONESHELL:
+.SHELLFLAGS: -euo
+.DEFAULT_GOAL: help
+.NOTPARALLEL:
+.POSIX:
+
+#
+
+# set default CMake generator
+ifeq ($(OS),Windows_NT)
+	export CMAKE_GENERATOR ?= Visual Studio 17 2022
+else ifeq ($(shell uname -s),Darwin)
+	export CMAKE_GENERATOR ?= Xcode
+else # Linux
+	export CMAKE_GENERATOR ?= Ninja
+endif
+
+CMAKE ?= cmake
+RM ?= $(CMAKE) -E rm -rf
+
+override DOTFILES_ROOT = $(patsubst %/,%,$(strip $(dir $(realpath $(firstword $(MAKEFILE_LIST))))))
+
+BUILDS_DIR ?= $(DOTFILES_ROOT)/Builds
+
+#
+
+.PHONY: help
+help:  ## Print this message
+	@grep -E '^[a-zA-Z_-]+:.*?\#\# .*$$' $(DOTFILES_ROOT)/Makefile | sort | awk 'BEGIN {FS = ":.*?\#\# "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+#
+
+$(BUILDS_DIR):
+	$(CMAKE) -S $(DOTFILES_ROOT) -B $(BUILDS_DIR)
+
+.PHONY: config
+config: $(BUILDS_DIR) ## Configure cmake
+
+#
+
+.PHONY: open
+open: config ## Open the dotfiles in an IDE
+	$(CMAKE) --open $(BUILDS_DIR)
+
+#
+
+.PHONY: clean
+clean: ## Clean the source tree
+	$(RM) $(BUILDS_DIR)
--- utilities/dotfiles.sh
+++ utilities/dotfiles.sh
@@ -2,34 +2,86 @@
 
 dotfiles_impl_dotfiles_dir="$(cd "$(dirname "$0")" && pwd)/.."
 
-# dotfiles
 #
-# dotfiles update
+
+dotfiles_print_help() {
+	cat << EOF
+
+dotfiles management command
+
+usage:
+
+	dotfiles clean
+		Cleanup temporary files.
+
+	dotfiles go
+		cd to the dotfiles git directory.
+
+	dotfiles help
+		Print this message.
+
+	dotfiles open
+		Open the dotfiles in an IDE.
+
+	dotfiles pull
+		Update the local copy of the dotfiles repo from git.
+
+	dotfiles update
+		Update the system dotfiles with links to the dotfiles downloaded from git.
+
+	dotfiles reload
+		Re-source the main.sh shell script.
+EOF
+}
+
 #
-# dotfiles help
+
+dotfiles_update() {
+	# add source/include lines to files in home dir
+	true;
+}
+
 #
-# dotfiles open
+
+dotfiles_pull() {
+	cd "$dotfiles_impl_dotfiles_dir" && git fetch && git pull
+	. "$dotfiles_impl_dotfiles_dir/main.sh"
+	dotfiles_update
+}
+
 #
-# dotfiles pull
+
+dotfiles_clean() {
+	brew cleanup
+	gem cleanup
+
+	cd "$dotfiles_impl_dotfiles_dir" && make clean
+}
+
 #
+
 dotfiles() {
 	case "$1" in
-		update)
-			# add source/include lines to files in home dir
+		clean)
+			dotfiles_clean
+			;;
+		go)
+			cd "$dotfiles_impl_dotfiles_dir" || exit 1
 			;;
 		help)
-			# print help
+			dotfiles_print_help
 			;;
 		open)
-			# open in IDE
+			cd "$dotfiles_impl_dotfiles_dir" && make open
 			;;
 		pull)
-			# git pull
-			# reload
-			dotfiles update
+			dotfiles_pull
+			;;
+		update)
+			dotfiles_update
 			;;
 		reload)
-			# resource dotfiles
+			# re-source dotfiles
 			. "$dotfiles_impl_dotfiles_dir/main.sh"
 			;;
 		*)
